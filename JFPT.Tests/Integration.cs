824f19330b39120875d15d09ae6291935dc8372e
banana <castiglione@mac.com>
2019-01-05 16:10:57 +0800

    add unit tests

---
+++ JFPT.Tests/Integration.cs
@@ -0,0 +1,12 @@
+using JFPT;
+using Xunit;
+
+public class Integration
+{
+    [Fact]
+    public void Test()
+    {
+        var page = new MainPage();
+        Assert.Equal("Welcome to Xamarin.Forms!", page.MainLabel.Text);
+    }
+}
